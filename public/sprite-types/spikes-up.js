77615cfcc1085ba44005013e90aebc4df6baed85
Janne Kivilahti <poppijanne@gmail.com>
2021-05-07 22:14:25 +0300

    prettier formatting

--- public/sprite-types/spikes-up.js
+++ public/sprite-types/spikes-up.js
@@ -1,29 +1,30 @@
+/* eslint-disable no-undef */
 spriteTypes["spikes-up"] = new SpriteType({
-    id: "spikes-up",
-    name: "Spikes Up",
-    shape: SHAPES.BOX,
-    shaded: false,
-    resizable: true,
-    collidesWithObstacles: false,
-    collisionBox: { t: 0.1, r: 0.8, b: 1, l: 0.2, },
-    spriteProps: {
-        weight: 0,
-        isObstacle: false,
-        maxVelocity: { x: 0, y: 0 }
+  id: "spikes-up",
+  name: "Spikes Up",
+  shape: SHAPES.BOX,
+  shaded: false,
+  resizable: true,
+  collidesWithObstacles: false,
+  collisionBox: { t: 0.1, r: 0.8, b: 1, l: 0.2 },
+  spriteProps: {
+    weight: 0,
+    isObstacle: false,
+    maxVelocity: { x: 0, y: 0 },
+  },
+  behaviours: ["kills-falling"],
+  paths: {
+    spikes: {
+      fill: "#d0d0d0",
+      stroke: "#303030",
+      commands: [
+        { c: "mt", x: 0.0, y: 1.0 },
+        { c: "lt", x: 0.25, y: 0.0 },
+        { c: "lt", x: 0.5, y: 1.0 },
+        { c: "lt", x: 0.75, y: 0.0 },
+        { c: "lt", x: 1.0, y: 1.0 },
+        { c: "cp" },
+      ],
     },
-    behaviours: ["kills-falling"],
-    paths: {
-        spikes: {
-            fill: "#d0d0d0",
-            stroke: "#303030",
-            commands: [
-                { c: "mt", x: 0.00, y: 1.0 },
-                { c: "lt", x: 0.25, y: 0.0 },
-                { c: "lt", x: 0.50, y: 1.0 },
-                { c: "lt", x: 0.75, y: 0.0 },
-                { c: "lt", x: 1.00, y: 1.0 },
-                { c: "cp" }
-            ]
-        }
-    }
+  },
 });
